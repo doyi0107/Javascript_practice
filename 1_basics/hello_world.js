a2267c50214bc8fa52958b82dfe2ba8d77650f8e
jsmme <doyi9917@naver.com>
2023-09-03 19:45:59 +0900

    loop train

---
+++ 1_basics/hello_world.js
@@ -0,0 +1,29 @@
+console.log('hi doyi');
+console.log(1*3);
+console.log(1*8*79);
+
+/** */
+
+const englishDay = 'friday';
+
+let koreanDay;
+
+switch('thursday'){
+    case 'monday':
+        koreanDay = '월요일';
+    case 'tuesday':
+        koreanDay = '화요일';
+    case 'wednesday':
+        koreanDay = '수요일';
+    case 'thursday':
+        koreanDay = '목요일';
+        console.log(koreanDay);
+    case 'friday':
+        koreanDay = '금요일';
+        console.log(koreanDay);
+    default:
+        koreanDay = '주말';
+        console.log(koreanDay);
+}
+
+console.log(koreanDay);
---
+++ 1_basics/loop.js
@@ -0,0 +1,19 @@
+const doyi = {
+    name : 'doyi',
+    year : 2023
+}
+
+/*객체에서 가져오면 키값*/
+
+for(let key in doyi){
+    console.log(key);
+}
+
+
+/*인덱스에서 가져오면 키는 인덱스값*/ 
+const ivememberArray = ['안유진','가을','이서','장원영','레이','리즈']
+
+for (let key in ivememberArray) {
+   console.log(key);
+}
+
